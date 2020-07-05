fe553ddbe0471479c902b83251fdf07160884635
Tyrone Lagore <tyronelagore.gmail.com>
2020-07-04 18:34:08 -0700

    send message if not sampling

--- ros2relay/network_publisher.py
+++ ros2relay/network_publisher.py
@@ -243,7 +243,7 @@ class NetworkPublisher(Node):
                     priorityItem = self.message_queue.get(True, 3)
                     topic = priorityItem.item.topic
                     self.metric_handler.increment_observed()
-                    if self.sampling:
+                    if self.sampling == True:
                         if self.worker_sample_counts[worker_id][topic] == self.topic_sample_rates[topic]:
                             self.send_message(priorityItem.item, worker_id)
                             self.worker_sample_counts[worker_id][topic] = 0
